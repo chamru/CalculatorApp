ef25f301f95d6e4c3064d32b01e2d99655216502
Softwarica College Lab <softwarica@softwarica.com>
2019-03-31 11:37:00 +0545

    gridlayout

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -2,8 +2,13 @@ package com.example.calculatorapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button zero,one,two,three,four,five,six,seven,eight,nine,decimal,equalsTo,
+            addition,subtraction,multiplication,division,percentage,reverseSign,cancel;
+
 
 
 
